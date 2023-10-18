d235e5757355dfba8d55a4ef74071e8ab2aed401
Svetlana Lutkova <lilissiya@yandex.ru>
2023-10-18 19:32:45 +0500

    Добавить код preloader из ТЗ

---
+++ src/components/Movies/Movies.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import SearchForm from '../SearchForm/SearchForm';
+
+function Movies() {
+
+  return (
+    <section className='movies'>
+      <SearchForm />
+      <h1 className='promo__title'>Учебный проект студента факультета Веб-разработки.</h1>
+      
+    </section>    
+  )
+}
+
+export default Movies;
