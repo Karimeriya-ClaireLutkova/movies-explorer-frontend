1dfd9656ac5af3007e007779714c6bba30c9cb7c
Svetlana Lutkova <lilissiya@yandex.ru>
2023-10-20 00:15:55 +0500

    Изменить компонент FilterCheckbox

--- src/components/FilterCheckbox/FilterCheckbox.js
+++ src/components/FilterCheckbox/FilterCheckbox.js
@@ -5,18 +5,12 @@ function FilterCheckbox() {
 
   return (
     <div className='filter-checkbox'>
-      <label class="form__label form__label_direction_right" for="one-column">
-        <input type="radio" class="form__item form__item_el_number-of-columns" name="number-of-columns" id="one-column" value="one-column" checked />
-        <span class="form__pseudo-item form__pseudo-item_type_radio"></span>
+      <label className='filter-checkbox__toggle'>
+        <input type='checkbox' className='filter-checkbox__input' />
+        <span className='filter-checkbox__slider'></span>
       </label>
-      <label class="form__label form__label_direction_right" for="two-columns">
-        <input type="radio" class="form__item form__item_el_number-of-columns" name="number-of-columns" id="two-columns" value="two-columns" />
-        <span class="form__pseudo-item form__pseudo-item_type_radio"></span>
-      </label>
-      <h3 class="form__heading form__heading_type_ctrl-el-heading">Короткометражки</h3>
-    </div>
-    
- 
+      <h3 className='filter-checkbox__subtitle'>Короткометражки</h3>
+    </div>   
   )
 }
 
