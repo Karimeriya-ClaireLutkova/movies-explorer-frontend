43779bfbe6c71a980af9e6a40993f8302090b94c
Svetlana Lutkova <lilissiya@yandex.ru>
2023-10-21 22:16:37 +0500

    Исправить в navigation

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,16 @@
 import logoHeader from '../../images/logo_header.svg';
 import Navigation from '../Navigation/Navigation';
-import { useLocation } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 
 function Header(props) {
   const {account, loggedIn, onAuthorization} = props;
   const { pathname } = useLocation();
   return (
-    <header className='header'>
-      <a class="header__link" href="/">
+    <header className="header">
+      <Link className="header__link" to="/" relative="path">
         <img className="header__logo" alt="Логотип Movies Explorer" src={logoHeader}/>
-      </a>
+      </Link>
       { (pathname === '/' || pathname === '/movies' || pathname === '/saved-movies') && <Navigation account={account} loggedIn={loggedIn} onAuthorization={onAuthorization}></Navigation>
       }
     </header>
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -7,19 +7,19 @@ function Navigation(props) {
 
   return (
     {loggedIn} ?
-    <nav className='navigate-autorized'>
-      <NavLink to='/' className='navigate-autorized__link'>Главная</NavLink>
-      <NavLink to='/movies' className='navigate-autorized__link'>Фильмы</NavLink>
-      <NavLink to='/saved-movies' className='navigate-autorized__link'>Сохранённые фильмы</NavLink>
-      <button type='button' className='navigate-autorized__button' onClick={onAuthorization}>
-          <span className=''>{account}</span>
-          <img className='navigate-autorized__logo' alt='Иконка аккаунта' src={imgAccount}/>
+    <nav className="navigate-autorized">
+      <NavLink to="/" className="navigate-autorized__link">Главная</NavLink>
+      <NavLink to="/movies" className="navigate-autorized__link">Фильмы</NavLink>
+      <NavLink to="/saved-movies" className="navigate-autorized__link">Сохранённые фильмы</NavLink>
+      <button type="button" className="navigate-autorized__button" onClick={onAuthorization}>
+          <span className="">{account}</span>
+          <img className="navigate-autorized__logo" alt="Иконка аккаунта" src={imgAccount}/>
       </button>    
     </nav>
     :
-    <nav className='navigate-access'>
-      <Link to='/signup' className='navigate-access__link'>Регистрация</Link>
-      <Link to='/signup' className='navigate-access__link'>Войти</Link>
+    <nav className="navigate-access">
+      <Link to="/signup" className="navigate-access__link">Регистрация</Link>
+      <Link to="/signup" className="navigate-access__link">Войти</Link>
     </nav>
   )
 }
