e81d3b80107b722ab0ae6ee927c67f71ad74e5c1
Svetlana Lutkova <lilissiya@yandex.ru>
2023-10-22 01:11:49 +0500

    Исправить техошибку

---
+++ src/components/Profile/Profile.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import PopupWithForm from '../PopupWithForm/PopupWithForm';
+import Header from '../Header/Header';
+
+export default function Register({isOpen, onSubmit, account, loggedIn, onAuthorization}) {
+  const [name, setName] = React.useState('');
+  const [userEmail, setUserEmail] = React.useState('');
+
+  function handleChangeName(evt) {
+    setName(evt.target.value);
+  }
+
+  function handleChangeEmail(evt) {
+    setUserEmail(evt.target.value);
+  }
+
+  function handleSubmit(evt) {
+    evt.preventDefault();
+    onSubmit(name, userEmail);
+    setUserEmail('');
+    setName('');
+  }
+
+  return (
+    <>
+      <Header id="4" account={account} loggedIn={loggedIn} onAuthorization={onAuthorization} />
+      <main>
+        <PopupWithForm id="1" name="registration-user" title="Добро пожаловать!"
+                       isOpen={isOpen}
+                       onSubmit={handleSubmit}
+                       buttonText={"Зарегистрироваться"}>
+          <div className="popup__field">
+            <p className="popup__input-text">Имя</p>
+            <input id="user-name-input" type="text" className="popup__input popup__input_type_entry" name="name" placeholder="Виталий" value={name} onChange={handleChangeName} required  />
+            <span className="user-name-input-error popup__input-error"></span>
+          </div>
+          <div className="popup__field">
+            <p className="popup__input-text">E-mail</p>
+            <input id="user-email-input" type="email" className="popup__input popup__input_type_entry" name="email" placeholder="Email" value={userEmail} onChange={handleChangeEmail} required  />
+            <span className="user-email-input-error popup__input-error"></span>
+          </div>
+          <div className="popup__field">
+            <p className="popup__input-text">Пароль</p>
+            <input id="user-password-input" type="password" className="popup__input popup__input_type_entry" name="password" placeholder="Пароль" value={password} onChange={handleChangePassword} required />
+            <span className="user-password-input-error popup__input-error"></span>
+          </div>
+          <div className="popup__signin">
+            <p className="popup__signin_title">Уже зарегистрированы?</p>
+            <Link to="/sign-in" className="popup__signin_link">Войти</Link>
+          </div>
+        </PopupWithForm>
+      </main>
+   </>
+  )
+}
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -31,30 +31,32 @@ export default function Register({isOpen, onSubmit}) {
   return (
     <>
       <Header id="5" />
-      <PopupWithForm id="2" name="registration-user" title="Добро пожаловать!"
-                     isOpen={isOpen}
-                     onSubmit={handleSubmit}
-                     buttonText={"Зарегистрироваться"}>
-        <div className="popup__field">
-          <p className="popup__input-text">Имя</p>
-          <input id="user-name-input" type="text" className="popup__input popup__input_type_entry" name="name" placeholder="Виталий" value={name} onChange={handleChangeName} required  />
-          <span className="user-name-input-error popup__input-error"></span>
-        </div>
-        <div className="popup__field">
-          <p className="popup__input-text">E-mail</p>
-          <input id="user-email-input" type="email" className="popup__input popup__input_type_entry" name="email" placeholder="Email" value={userEmail} onChange={handleChangeEmail} required  />
-          <span className="user-email-input-error popup__input-error"></span>
-        </div>
-        <div className="popup__field">
-          <p className="popup__input-text">Пароль</p>
-          <input id="user-password-input" type="password" className="popup__input popup__input_type_entry" name="password" placeholder="Пароль" value={password} onChange={handleChangePassword} required />
-          <span className="user-password-input-error popup__input-error"></span>
-        </div>
-        <div className="popup__signin">
-          <p className="popup__signin_title">Уже зарегистрированы?</p>
-          <Link to="/sign-in" className="popup__signin_link">Войти</Link>
-        </div>
-      </PopupWithForm>
+      <main>
+        <PopupWithForm id="2" name="registration-user" title="Добро пожаловать!"
+                       isOpen={isOpen}
+                       onSubmit={handleSubmit}
+                       buttonText={"Зарегистрироваться"}>
+          <div className="popup__field">
+            <p className="popup__input-text">Имя</p>
+            <input id="user-name-input" type="text" className="popup__input popup__input_type_entry" name="name" placeholder="Виталий" value={name} onChange={handleChangeName} required  />
+            <span className="user-name-input-error popup__input-error"></span>
+          </div>
+          <div className="popup__field">
+            <p className="popup__input-text">E-mail</p>
+            <input id="user-email-input" type="email" className="popup__input popup__input_type_entry" name="email" placeholder="Email" value={userEmail} onChange={handleChangeEmail} required  />
+            <span className="user-email-input-error popup__input-error"></span>
+          </div>
+          <div className="popup__field">
+            <p className="popup__input-text">Пароль</p>
+            <input id="user-password-input" type="password" className="popup__input popup__input_type_entry" name="password" placeholder="Пароль" value={password} onChange={handleChangePassword} required />
+            <span className="user-password-input-error popup__input-error"></span>
+          </div>
+          <div className="popup__signin">
+            <p className="popup__signin_title">Уже зарегистрированы?</p>
+            <Link to="/sign-in" className="popup__signin_link">Войти</Link>
+          </div>
+        </PopupWithForm>
+      </main>
    </>
   )
 }
