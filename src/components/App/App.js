50dbb05d8d3eb28f4baddbb27f6430c07c2a567d
Svetlana Lutkova <lilissiya@yandex.ru>
2023-10-22 01:18:33 +0500

    Исправить техошибку в пути к компоненту SavedMovies

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,7 +4,7 @@ import Main from '../Main/Main';
 import Footer from '../Footer/Footer';
 import NotFoundPage from '../NotFoundPage/NotFoundPage';
 import Movies from '../Movies/Movies';
-import SavedMovies from '../SavedMovies';
+import SavedMovies from '../SavedMovies/SavedMovies';
 import Profile from '../Profile/Profile';
 import Register from '../Register/Register';
 import Login from '../Login/Login';
