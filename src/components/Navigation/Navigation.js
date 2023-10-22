8b5fb3ee0c96d6a180d7af4591162cac19c06bdb
Svetlana Lutkova <lilissiya@yandex.ru>
2023-10-23 02:04:47 +0500

    Исправить техошибку в Navigation

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -5,21 +5,24 @@ import imgAccount from '../../images/img_account.svg';
 function Navigation({account, loggedIn, onAuthorization}) {
 
   return (
-    {loggedIn} ?
-    <nav className="navigate-autorized">
-      <NavLink to="/" className="navigate-autorized__link">Главная</NavLink>
-      <NavLink to="/movies" className="navigate-autorized__link">Фильмы</NavLink>
-      <NavLink to="/saved-movies" className="navigate-autorized__link">Сохранённые фильмы</NavLink>
-      <button type="button" className="navigate-autorized__button" onClick={onAuthorization}>
-          <span className="">{account}</span>
-          <img className="navigate-autorized__logo" alt="Иконка аккаунта" src={imgAccount}/>
-      </button>    
-    </nav>
-    :
-    <nav className="navigate-access">
-      <Link to="/signup" className="navigate-access__link">Регистрация</Link>
-      <Link to="/signup" className="navigate-access__link">Войти</Link>
-    </nav>
+    <div>
+      {loggedIn ? (
+        <nav className="navigate-autorized">
+          <NavLink to="/" className="navigate-autorized__link">Главная</NavLink>
+          <NavLink to="/movies" className="navigate-autorized__link">Фильмы</NavLink>
+          <NavLink to="/saved-movies" className="navigate-autorized__link">Сохранённые фильмы</NavLink>
+          <button type="button" className="navigate-autorized__button" onClick={onAuthorization}>
+            <span className="">{account}</span>
+            <img className="navigate-autorized__logo" alt="Иконка аккаунта" src={imgAccount}/>
+          </button>    
+        </nav>
+      ) : (
+        <nav className="navigate-access">
+          <Link to="/signup" className="navigate-access__link">Регистрация</Link>
+          <Link to="/signup" className="navigate-access__link">Войти</Link>
+        </nav>
+      )}
+    </div>    
   )
 }
 
