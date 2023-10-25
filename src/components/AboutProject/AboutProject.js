98280b2ee1165aaecee5201d9aa74337383fba74
Svetlana Lutkova <lilissiya@yandex.ru>
2023-10-25 23:38:28 +0500

    Исправить техошибку в Promo

--- src/components/AboutProject/AboutProject.js
+++ src/components/AboutProject/AboutProject.js
@@ -1,26 +1,27 @@
 import React from 'react';
+import './AboutProject.css';
 
 function AboutProject() {
 
   return (
-    <section className='about-project' aria-label="Две колонки">
-      <h2 className='section__title'>О проекте</h2>
-      <article className='two-columns'>
-        <h3 className='two-columns__brief'>Дипломный проект включал 5 этапов</h3>
-        <p className='two-columns__paragraph'>Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.</p>
+    <section className="about-project" aria-label="Две колонки">
+      <h2 className="section__title">О проекте</h2>
+      <article className="about-project__stages">
+        <h3 className="about-project__brief">Дипломный проект включал 5 этапов</h3>
+        <p className="about-project__paragraph">Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.</p>
       </article>
-      <article className='two-columns'>
-        <h3 className='two-columns__brief'>На выполнение диплома ушло 5 недель</h3>
-        <p className='two-columns__paragraph'>У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.</p>
+      <article className="about-project__time">
+        <h3 className="about-project__brief">На выполнение диплома ушло 5 недель</h3>
+        <p className="about-project__paragraph">У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.</p>
       </article>
-      <article className='two-columns'>
-        <div className='two-columns__scheme'>
-          <h3 className='two-columns__brief'>1 неделя</h3>
-          <p className='two-columns__paragraph'>Back-end</p>
+      <article className="about-project__distribution">
+        <div className="about-project__scheme">
+          <h3 className="about-project__brief about-project__brief_type_decrease">1 неделя</h3>
+          <p className="about-project__paragraph about-project__paragraph_type_distribution">Back-end</p>
         </div>
-        <div className='two-columns__scheme'>
-          <h3 className='two-columns__brief'>4 недели</h3>
-          <p className='two-columns__paragraph'>Front-end</p>
+        <div className="about-project__scheme">
+          <h3 className="about-project__brief about-project__brief_type_increase">4 недели</h3>
+          <p className="about-project__paragraph about-project__paragraph_type_distribution">Front-end</p>
         </div>
       </article>
     </section>    
