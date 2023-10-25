b968fd2912e1c6095e6a108b31a7898d647c60fb
Svetlana Lutkova <lilissiya@yandex.ru>
2023-10-25 16:06:09 +0500

    Добавить css-код для navigation при отсутствии авторизации

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -8,32 +8,34 @@ function Navigation({account, loggedIn, onAuthorization, onNavigation}) {
   let classNameNavActive = "navigate-autorized__link navigate-autorized__link_type_active";
 
   return (
-    <div className="navigate-autorized__container">
+    <>
       {loggedIn ? (
-        <nav className="navigate-autorized" id="navBar">
-          <button onClick={onNavigation} className="navigate-autorized__dropdown">
-            <img className="navigate-autorized__icon" alt="Три черты выпадающего списка навигации" src={dropdownNavBar} />
-          </button>
-          <button type="button" className="navigate-autorized__button navigate-autorized__button_type_close" onClick={onNavigation}></button>
-          <div className="navigate-autorized__list">
-            <NavLink to="/" className={({ isActive }) => isActive ? `${classNameNavActive} navigate-autorized__link_hidden`: "navigate-autorized__link navigate-autorized__link_hidden"} onClick={onNavigation}>Главная</NavLink>
-            <NavLink to="/movies" className={({ isActive }) => isActive ? classNameNavActive : "navigate-autorized__link"} onClick={onNavigation} >Фильмы</NavLink>
-            <NavLink to="/saved-movies" className={({ isActive }) => isActive ? classNameNavActive : "navigate-autorized__link"} onClick={onNavigation}>Сохраненные фильмы</NavLink>
-          </div>          
-          <button type="button" className="navigate-autorized__button navigate-autorized__button_type_account" onClick={onAuthorization}>
-            <p className="navigate-autorized_text">{account}</p>
-            <div className="navigate-autorized__container-logo">
-              <img className="navigate-autorized__logo" alt="Иконка аккаунта" src={imgAccount}/>
-            </div>            
-          </button>
-        </nav>
+        <div className="navigate-autorized__container">
+          <nav className="navigate-autorized" id="navBar">
+            <button onClick={onNavigation} className="navigate-autorized__dropdown">
+              <img className="navigate-autorized__icon" alt="Три черты выпадающего списка навигации" src={dropdownNavBar} />
+            </button>
+            <button type="button" className="navigate-autorized__button navigate-autorized__button_type_close" onClick={onNavigation}></button>
+            <div className="navigate-autorized__list">
+              <NavLink to="/" className={({ isActive }) => isActive ? `${classNameNavActive} navigate-autorized__link_hidden`: "navigate-autorized__link navigate-autorized__link_hidden"} onClick={onNavigation}>Главная</NavLink>
+              <NavLink to="/movies" className={({ isActive }) => isActive ? classNameNavActive : "navigate-autorized__link"} onClick={onNavigation} >Фильмы</NavLink>
+              <NavLink to="/saved-movies" className={({ isActive }) => isActive ? classNameNavActive : "navigate-autorized__link"} onClick={onNavigation}>Сохраненные фильмы</NavLink>
+            </div>          
+            <button type="button" className="navigate-autorized__button navigate-autorized__button_type_account" onClick={onAuthorization}>
+              <p className="navigate-autorized_text">{account}</p>
+              <div className="navigate-autorized__container-logo">
+                <img className="navigate-autorized__logo" alt="Иконка аккаунта" src={imgAccount}/>
+              </div>            
+            </button>
+          </nav>
+        </div>
       ) : (
         <nav className="navigate-access">
           <Link to="/sign-up" className="navigate-access__link">Регистрация</Link>
           <Link to="/sign-in" className="navigate-access__link">Войти</Link>
         </nav>
       )}
-    </div>    
+    </>    
   )
 }
 
