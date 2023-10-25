65b88091200ffabb851c40c26feafd59bf2b0cce
Svetlana Lutkova <lilissiya@yandex.ru>
2023-10-25 23:19:25 +0500

    Стилизовать AboutProject

--- src/components/AboutProject/AboutProject.js
+++ src/components/AboutProject/AboutProject.js
@@ -5,25 +5,28 @@ function AboutProject() {
 
   return (
     <section className="about-project" aria-label="Две колонки">
-      <h2 className="section__title">О проекте</h2>
-      <article className="about-project__stages">
-        <h3 className="about-project__brief">Дипломный проект включал 5 этапов</h3>
-        <p className="about-project__paragraph">Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.</p>
-      </article>
-      <article className="about-project__time">
-        <h3 className="about-project__brief">На выполнение диплома ушло 5 недель</h3>
-        <p className="about-project__paragraph">У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.</p>
-      </article>
-      <article className="about-project__distribution">
-        <div className="about-project__scheme">
-          <h3 className="about-project__brief about-project__brief_type_decrease">1 неделя</h3>
-          <p className="about-project__paragraph about-project__paragraph_type_distribution">Back-end</p>
-        </div>
-        <div className="about-project__scheme">
-          <h3 className="about-project__brief about-project__brief_type_increase">4 недели</h3>
-          <p className="about-project__paragraph about-project__paragraph_type_distribution">Front-end</p>
-        </div>
-      </article>
+      <div className="about-project__container">
+        <h2 className="section__title">О проекте</h2>
+        <article className="about-project__stages">
+          <h3 className="about-project__brief">Дипломный проект включал 5 этапов</h3>
+          <p className="about-project__paragraph">Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.</p>
+        </article>
+        <article className="about-project__time">
+          <h3 className="about-project__brief">На выполнение диплома ушло 5 недель</h3>
+          <p className="about-project__paragraph">У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.</p>
+        </article>
+        <article className="about-project__distribution">
+          <div className="about-project__scheme">
+            <h3 className="about-project__brief about-project__brief_type_decrease">1 неделя</h3>
+            <p className="about-project__paragraph about-project__paragraph_type_distribution">Back-end</p>
+          </div>
+          <div className="about-project__scheme">
+            <h3 className="about-project__brief about-project__brief_type_increase">4 недели</h3>
+            <p className="about-project__paragraph about-project__paragraph_type_distribution">Front-end</p>
+          </div>
+        </article>
+      </div>
+      
     </section>    
   )
 }
