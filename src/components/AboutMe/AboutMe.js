cdcc7fd3feb8397a8f6bb837d0e525505b170a1d
Svetlana Lutkova <lilissiya@yandex.ru>
2023-10-27 01:11:33 +0500

    Стилизовать AboutMe

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -1,22 +1,28 @@
 import React from 'react';
 import avatarStudent from '../../images/avatar_student.jpg';
 import Portfolio from '../Portfolio/Portfolio';
+import './AboutMe.css';
 
 function AboutMe() {
 
   return (
-    <section className='about-me' aria-label='Две части'>
-      <h2 className='section__title'>Студент</h2>
-      <article className='about-me__info'>
-        <h3 className='section__subtitle'>Виталий</h3>
-        <p className='section__subline'>Фронтенд-разработчик, 30 лет</p>
-        <p className='section__paragraph'>Я родился и живу в Саратове, закончил факультет экономики СГУ.
-         У меня есть жена и дочь. Я люблю слушать музыку, а ещё увлекаюсь бегом. Недавно начал кодить. 
-         С 2015 года работал в компании «СКБ Контур». После того, как прошёл курс по веб-разработке, 
-         начал заниматься фриланс-заказами и ушёл с постоянной работы.</p>
-        <a className='about-me__link' href='https://github.com/Karimeriya-ClaireLutkova' target='_blank' rel='noreferrer'>Github</a>
-        <img className='about-me__image' src={avatarStudent} alt='Фотография студента'/>
-      </article>
+    <section className="about-me" aria-label="Две части">
+      <h2 className="section__title">Студент</h2>
+      <div className="about-me__container">
+        <div className="about-me__info">
+          <h3 className="section__subtitle section__subtitle_type_about-me">Виталий</h3>
+          <p className="section__subline">Фронтенд-разработчик, 30 лет</p>
+          <p className="section__paragraph section__paragraph_type_about-me">Я родился и живу в Саратове, закончил факультет экономики СГУ.
+            У меня есть жена и&nbsp;дочь. Я люблю слушать музыку, а ещё увлекаюсь бегом. Недавно начал кодить. 
+            С 2015 года работал в компании «СКБ Контур». После того, как прошёл курс по веб-разработке, 
+            начал заниматься фриланс-заказами и ушёл с постоянной работы.
+          </p>
+          <a className="about-me__link" href="https://github.com/Karimeriya-ClaireLutkova" target="_blank" rel="noreferrer">Github</a>
+        </div> 
+        <div className="about-me__avatar">
+          <img className="about-me__image" src={avatarStudent} alt="Фотография студента"/>        
+        </div>      
+      </div>
       <Portfolio />      
     </section>    
   )
