a92cea6272805302ccbea593d6370a9c894cf70a
Svetlana Lutkova <lilissiya@yandex.ru>
2023-10-27 02:22:03 +0500

    Стилизовать aboutMe на 768

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -14,7 +14,7 @@ function AboutMe() {
           <p className="section__subline">Фронтенд-разработчик, 30 лет</p>
           <p className="section__paragraph section__paragraph_type_about-me">Я родился и живу в Саратове, закончил факультет экономики СГУ.
             У меня есть жена и&nbsp;дочь. Я люблю слушать музыку, а ещё увлекаюсь бегом. Недавно начал кодить. 
-            С 2015 года работал в компании «СКБ Контур». После того, как прошёл курс по веб-разработке, 
+            С&nbsp;2015 года работал в компании «СКБ Контур». После того, как прошёл курс по веб-разработке, 
             начал заниматься фриланс-заказами и ушёл с постоянной работы.
           </p>
           <a className="about-me__link" href="https://github.com/Karimeriya-ClaireLutkova" target="_blank" rel="noreferrer">Github</a>
--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,28 +1,29 @@
 import React from 'react';
 import arrowLink from '../../images/arrow_portfolio.svg';
+import './Portfolio.css';
 
 function Portfolio() {
   
   return (
-    <section className='portfolio'>
-      <p className='section__subline'>Портфолио</p>
-      <ul className='portfolio__list'>
-        <li className='portfolio__list-item'>
-          <a className='portfolio__link' href='https://github.com/Karimeriya-ClaireLutkova/how-to-learn' target='_blank' rel="noreferrer">
-            <h3 className='section__subtitle'>Статичный сайт</h3>
-            <img className='portfolio__arrow' src={arrowLink} alt='Стрелка вверх вправо'/>
+    <section className="portfolio">
+      <p className="section__subline section__subline_type_portfolio">Портфолио</p>
+      <ul className="portfolio__list">
+        <li className="portfolio__list-item">
+          <a className="portfolio__link" href="https://github.com/Karimeriya-ClaireLutkova/how-to-learn" target="_blank" rel="noreferrer">
+            <h3 className="section__subtitle">Статичный сайт</h3>
+            <img className="portfolio__arrow" src={arrowLink} alt="Стрелка вверх вправо"/>
           </a>
         </li>
-        <li className='portfolio__list-item'>
-          <a className='portfolio__link' href='https://github.com/Karimeriya-ClaireLutkova/russian-travel' target='_blank' rel="noreferrer">
-            <h3 className='section__subtitle'>Адаптивный сайт</h3>
-            <img className='portfolio__arrow' src={arrowLink} alt='Стрелка вверх вправо'/>
+        <li className="portfolio__list-item">
+          <a className="portfolio__link" href="https://github.com/Karimeriya-ClaireLutkova/russian-travel" target="_blank" rel="noreferrer">
+            <h3 className="section__subtitle">Адаптивный сайт</h3>
+            <img className="portfolio__arrow" src={arrowLink} alt="Стрелка вверх вправо"/>
           </a>
         </li>
-        <li className='portfolio__list-item'>
-        <a className='portfolio__link' href='https://github.com/Karimeriya-ClaireLutkova/react-mesto-api-full-gha' target='_blank' rel="noreferrer">
-            <h3 className='section__subtitle'>Одностраничное приложение</h3>
-            <img className='portfolio__arrow' src={arrowLink} alt='Стрелка вверх вправо'/>
+        <li className="portfolio__list-item">
+        <a className="portfolio__link" href="https://github.com/Karimeriya-ClaireLutkova/react-mesto-api-full-gha" target="_blank" rel="noreferrer">
+            <h3 className="section__subtitle">Одностраничное приложение</h3>
+            <img className="portfolio__arrow" src={arrowLink} alt="Стрелка вверх вправо"/>
           </a>
         </li>
       </ul>
