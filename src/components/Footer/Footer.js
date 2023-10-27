738ec60837d29e5d41a69c3f7690f4c50d66f00f
Svetlana Lutkova <lilissiya@yandex.ru>
2023-10-27 18:37:19 +0500

    Исправить техошибку

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -8,7 +8,7 @@ function Footer() {
       <h2 className="footer__title">Учебный проект Яндекс.Практикум х BeatFilm.</h2>
       <div className="footer__container">
         <p className="footer__copyright">&#169; 2023</p>
-        <nav>
+        <nav className="footer__nav">
           <ul className="footer__links">
             <li>
               <a className="footer__link" href="https://practicum.yandex.ru" target="_blank" rel="noreferrer">Яндекс.Практикум</a>
