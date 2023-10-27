41fa47af06425911be78b7d1fcc05641300efaaf
Svetlana Lutkova <lilissiya@yandex.ru>
2023-10-27 18:35:49 +0500

    Стилизовать search

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,23 +1,24 @@
 import React from 'react';
-import loupeIcon from '../../images/loupe_icon.svg';
 import FilterCheckbox from '../FilterCheckbox/FilterCheckbox';
 import './SearchForm.css';
 
 function SearchForm() {
 
   return (
-    <section className="search-form">
-      <form className="search-form__form">
-        <img className="search-form__img" alt="Изображение лупы" src={loupeIcon}/>
-        <div className="search-form__form-search">
+    <section className="search">
+      <form className="search-form">
+        <div className="search-form__container">
           <div className="search-form__field">
+            <div className="search-form__icon"></div>
             <input id="movies-data-input" type="text" className="search-form__input search-form__input_type_entry" name="film" placeholder="Фильм" required  />
             <span className="movies-data-input-error search-form__input-error"></span>
           </div>
+          <button type="submit" className="search-form__button">Найти</button>
         </div>
-        <button type="submit" className="search-form__button">Найти</button>
-      </form>
-      <FilterCheckbox />
+        <div className="search-form__filter">
+            <FilterCheckbox />
+        </div>
+      </form>      
     </section>    
   )
 }
