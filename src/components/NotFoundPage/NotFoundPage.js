1bd9be91fbe8718c46204613c293c81811532089
Svetlana Lutkova <lilissiya@yandex.ru>
2023-10-27 15:06:20 +0500

    Стилизовать страницу NotFound

--- src/components/NotFoundPage/NotFoundPage.js
+++ src/components/NotFoundPage/NotFoundPage.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import './NotFoundPage.css';
 
 function NotFoundPage({onBack}) {
 
   return (
     <main>
       <section className="missing-page">
-        <h1 className="missing-page__title">404</h1>
-        <h2 className="missing-page__title">Страница не найдена</h2>
-        <button type="button" className="missing-page__button" onClick={onBack}>Назад</button>
+        <div className="missing-page__container">
+          <h1 className="missing-page__title">404</h1>
+          <h2 className="missing-page__subtitle">Страница не найдена</h2>
+          <button type="button" className="missing-page__button" onClick={onBack}>Назад</button>  
+        </div>
       </section>   
     </main>     
   )
