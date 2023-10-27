afb142a193de1b0d4cde850828c22ad5cca33bb0
Svetlana Lutkova <lilissiya@yandex.ru>
2023-10-27 18:23:10 +0500

    Исправить код AuboutProject

--- src/components/AboutProject/AboutProject.js
+++ src/components/AboutProject/AboutProject.js
@@ -5,17 +5,17 @@ function AboutProject() {
 
   return (
     <section className="about-project" aria-label="Две колонки">
+      <h2 className="section__title section__title_type_about-project">О проекте</h2>
       <div className="about-project__container">
-        <h2 className="section__title section__title_type_about-project">О проекте</h2>
-        <article className="about-project__stages">
+        <article className="stages">
           <h3 className="about-project__brief">Дипломный проект включал 5 этапов</h3>
           <p className="about-project__paragraph">Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.</p>
         </article>
-        <article className="about-project__time">
+        <article className="stages">
           <h3 className="about-project__brief">На выполнение диплома ушло 5 недель</h3>
           <p className="about-project__paragraph">У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.</p>
         </article>
-        <article className="about-project__distribution">
+        <article className="distribution">
           <div className="about-project__scheme">
             <h3 className="about-project__brief about-project__brief_type_decrease">1 неделя</h3>
             <p className="about-project__paragraph about-project__paragraph_type_distribution">Back-end</p>
@@ -24,9 +24,9 @@ function AboutProject() {
             <h3 className="about-project__brief about-project__brief_type_increase">4 недели</h3>
             <p className="about-project__paragraph about-project__paragraph_type_distribution">Front-end</p>
           </div>
-        </article>
+        </article>        
       </div>      
-    </section> 
+    </section>
   )
 }
 
