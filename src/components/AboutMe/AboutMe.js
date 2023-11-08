bef6d600297ef2393758a696d8bddfe3be7862ee
Svetlana Lutkova <lilissiya@yandex.ru>
2023-11-08 12:18:25 +0500

    Исправить по бэм portfolio, about me, about project

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -7,22 +7,22 @@ function AboutMe() {
 
   return (
     <section className="about-me" aria-label="Две части">
-      <h2 className="section__title">Студент</h2>
+      <h2 className="title">Студент</h2>
       <div className="about-me__container">
         <div className="about-me__info">
-          <h3 className="section__subtitle section__subtitle_type_about-me">Виталий</h3>
-          <p className="section__subline section__subline_type_about-me">Фронтенд-разработчик, 30 лет</p>
-          <p className="section__paragraph section__paragraph_type_about-me">Я родился и живу в Саратове, закончил факультет экономики СГУ.
+          <h3 className="subtitle subtitle_about-me">Виталий</h3>
+          <p className="subline subline_about-me">Фронтенд-разработчик, 30 лет</p>
+          <p className="paragraph paragraph_about-me">Я родился и живу в Саратове, закончил факультет экономики СГУ.
             У меня есть жена и&nbsp;дочь. Я люблю слушать музыку, а ещё увлекаюсь бегом. Недавно начал кодить.
             С&nbsp;2015 года работал в компании «СКБ Контур». После того, как прошёл курс по веб-разработке,
             начал заниматься фриланс-заказами и ушёл с постоянной работы.
           </p>
           <a className="about-me__link" href="https://github.com/Karimeriya-ClaireLutkova" target="_blank" rel="noreferrer">Github</a>
         </div>
-        <img className="about-me__image" src={avatarStudent} alt="Фотография студента"/>     
+        <img className="about-me__image" src={avatarStudent} alt="Фотография студента"/>   
       </div>
-      <Portfolio />    
-    </section> 
+      <Portfolio />
+    </section>
   )
 }
 
--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -5,23 +5,23 @@ function Portfolio() {
 
   return (
     <section className="portfolio">
-      <p className="section__subline section__subline_type_portfolio">Портфолио</p>
+      <p className="subline subline_portfolio">Портфолио</p>
       <ul className="portfolio__list">
         <li className="portfolio__list-item">
           <a className="portfolio__link" href="https://github.com/Karimeriya-ClaireLutkova/how-to-learn" target="_blank" rel="noreferrer">
-            <h3 className="section__subtitle section__subtitle_type_portfolio">Статичный сайт</h3>
+            <h3 className="subtitle subtitle_portfolio">Статичный сайт</h3>
             <div className="portfolio__arrow"></div>
           </a>
         </li>
         <li className="portfolio__list-item">
           <a className="portfolio__link" href="https://github.com/Karimeriya-ClaireLutkova/russian-travel" target="_blank" rel="noreferrer">
-            <h3 className="section__subtitle section__subtitle_type_portfolio">Адаптивный сайт</h3>
+            <h3 className="subtitle subtitle_portfolio">Адаптивный сайт</h3>
             <div className="portfolio__arrow"></div>
           </a>
         </li>
-        <li className="portfolio__list-item portfolio__list-item_type_last-child">
+        <li className="portfolio__list-item portfolio__list-item_last-child">
         <a className="portfolio__link" href="https://github.com/Karimeriya-ClaireLutkova/react-mesto-api-full-gha" target="_blank" rel="noreferrer">
-            <h3 className="section__subtitle section__subtitle_type_portfolio">Одностраничное приложение</h3>
+            <h3 className="subtitle subtitle_portfolio">Одностраничное приложение</h3>
             <div className="portfolio__arrow"></div>
         </a>
         </li>
